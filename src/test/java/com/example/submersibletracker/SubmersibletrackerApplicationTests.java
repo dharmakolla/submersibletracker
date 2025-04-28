3d4b1e3b49a2d98dd0f690fb12b4877e3c91f757
dharmakolla <dharmakolla85@gmail.com>
2025-04-28 23:00:13 +0530

    Updated Test cases.

--- src/test/java/com/example/submersibletracker/SubmersibletrackerApplicationTests.java
+++ src/test/java/com/example/submersibletracker/SubmersibletrackerApplicationTests.java
@@ -1,13 +1,75 @@
 package com.example.submersibletracker;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import java.util.Arrays;
 
-@SpringBootTest
+import org.junit.jupiter.api.AfterEach;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.springframework.http.ResponseEntity;
+
 class SubmersibletrackerApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	@Mock
+    private SubmersibletrackerController controller;
 
+	@Mock
+	private Submersible submersible;
+
+    @BeforeEach
+    public void setUp() {
+		submersible = new Submersible("Submersible", 0, 0);
+		controller = new SubmersibletrackerController(submersible);
+		
+    }
+
+    @AfterEach
+    public void tearDown() {
+        controller = null;
+    }
+
+    @Test   
+    public void testMoveLeft() {
+		submersible = new Submersible("Submersible", 0, 0);
+        ResponseEntity<Submersible> response = controller.moveLeft(Arrays.asList(1,0));
+        assertTrue(response.getStatusCode().is2xxSuccessful());
+        assertEquals(-1, response.getBody().getX());
+        assertEquals(0, response.getBody().getY());
+    }
+
+    @Test
+    public void testMoveRight() {
+		submersible = new Submersible("Submersible", 0, 0);
+        ResponseEntity<Submersible> response = controller.moveRight(Arrays.asList(1,0));
+        assertTrue(response.getStatusCode().is2xxSuccessful());
+        assertEquals(1, response.getBody().getX());
+        assertEquals(0, response.getBody().getY());
+    }
+
+    @Test
+    public void testMoveUp() {
+		submersible = new Submersible("Submersible", 0, 0);
+        ResponseEntity<Submersible> response = controller.moveUp(Arrays.asList(0,1));
+        assertTrue(response.getStatusCode().is2xxSuccessful());
+        assertEquals(0, response.getBody().getX());
+        assertEquals(1, response.getBody().getY());
+    }
+
+    @Test
+    public void testMoveDown() {
+		submersible = new Submersible("Submersible", 0, 0);
+        ResponseEntity<Submersible> response = controller.moveDown(Arrays.asList(0,1));
+        assertTrue(response.getStatusCode().is2xxSuccessful());
+        assertEquals(0, response.getBody().getX()); 
+        assertEquals(-1, response.getBody().getY());    
+    }
+
+   /*  @Test
+    public void testSetObstacles() {
+        controller.setObstacles(Arrays.asList(Arrays.asList()));
+        assertNotEquals(1, submersible.getX());
+        assertNotEquals(2, submersible.getY());
+    } */
 }
