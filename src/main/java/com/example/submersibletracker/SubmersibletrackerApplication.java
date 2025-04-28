755ab82134ed434d4bd885fc873a94d914f7feda
dharmakolla <dharmakolla85@gmail.com>
2025-04-28 12:42:50 +0530

    Initial commit for submersible tracker.

---
+++ src/main/java/com/example/submersibletracker/SubmersibletrackerApplication.java
@@ -0,0 +1,13 @@
+package com.example.submersibletracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubmersibletrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SubmersibletrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/submersibletracker/SubmersibletrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.submersibletracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubmersibletrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
